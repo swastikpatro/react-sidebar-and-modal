87a046f2e9af6cf56c30960041ae6291a788d3fd
swastikpatro <patroswastik.jsdev@gmail.com>
2022-12-28 20:27:03 +0530

    fix: links fixed

--- sidebar-and-modal-project/src/data.tsx
+++ sidebar-and-modal-project/src/data.tsx
@@ -19,25 +19,25 @@ export const links = [
   },
   {
     id: 2,
-    url: '/team',
+    url: 'https://github.com/swastikpatro',
     text: 'team',
     icon: <FaUserFriends />,
   },
   {
     id: 3,
-    url: '/projects',
+    url: 'https://github.com/swastikpatro?tab=repositories',
     text: 'projects',
     icon: <FaFolderOpen />,
   },
   {
     id: 4,
-    url: '/calendar',
+    url: 'https://github.com/swastikpatro',
     text: 'calendar',
     icon: <FaCalendarAlt />,
   },
   {
     id: 5,
-    url: '/documents',
+    url: 'https://github.com/swastikpatro',
     text: 'documents',
     icon: <FaWpforms />,
   },
@@ -46,27 +46,27 @@ export const links = [
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: 'https://twitter.com/Swastik2001',
     icon: <FaFacebook />,
   },
   {
     id: 2,
-    url: 'https://www.twitter.com',
+    url: 'https://twitter.com/Swastik2001',
     icon: <FaTwitter />,
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com/in/swastik-patro-2a54bb19b/',
     icon: <FaLinkedin />,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com/in/swastik-patro-2a54bb19b/',
     icon: <FaBehance />,
   },
   {
     id: 5,
-    url: 'https://www.twitter.com',
+    url: 'https://github.com/swastikpatro',
     icon: <FaSketch />,
   },
 ];
